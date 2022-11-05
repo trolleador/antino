1870587eae245987439a45224dcecba1bc5089ff
unknown <Apoyo a Estudiantes@8A-DISE-CECY.uson.mx>
2022-11-04 17:57:14 -0700

    2

--- pygame/prueba.py
+++ pygame/prueba.py
@@ -1,3 +1,4 @@
+#importamos la biblioteca XD
 import pygame
 from pygame.locals import *
 import sys
@@ -6,29 +7,31 @@ import sys
 #iniciadores
 pygame.init()
 
-HEIGHT = 450
-WIDTH = 400
+HEIGHT = 800
+WIDTH = 800
 FPS = 60
 
 #ventana 
 displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
 
-pygame.display.set_caption("Game")
+pygame.display.set_caption("Never gonna give you up Never gonna let you down Never gonna run around and desert you")
  
 FramePerSec = pygame.time.Clock()
 
 x = 0
+y = 0
 
 rect = pygame.Rect(20,20,20,20)
 
 #imagen
 
-zorro = pygame.image.load('pygame\zorro nft.png')
+zorro = pygame.image.load("C:/Users/Cecilia/Desktop/yo/pygame/zorro nft.png")
 
 #loop principal:
 
 while True:
 
+   #Se necesita rellenar toda la pantalla con un color para que el frame anterior no salga como fondo
    displaysurface.fill((0,0,0))
 
    #cierra el progama si aplastas la "x" en la ventana
@@ -39,21 +42,25 @@ while True:
 
     
 
+   #checa que teclas están oprimidas
    pressed = pygame.key.get_pressed()
-   if pressed[pygame.K_w]:
-      rect.y -= 1
-   if pressed[pygame.K_s]:
-      rect.y += 1
-   if pressed[pygame.K_a]:
-      rect.x -= 1
-   if pressed[pygame.K_d]:
-      rect.x += 1
 
+   #movimiento
+   if pressed[pygame.K_w]:
+      rect.y -= 2
+   if pressed[pygame.K_s]:
+      rect.y += 2
+   if pressed[pygame.K_a]:
+      rect.x -= 2
+   if pressed[pygame.K_d]:
+      rect.x += 2
+
+
+   #renderizado
    pygame.draw.rect(displaysurface, (255,0,0), rect)
 
    displaysurface.blit(zorro, (rect.x,rect.y))
-
-   x += .1
+   
    #actualiza lo que se ve en ventana
    pygame.display.update()
 
