2b86e6b84ed426a4ac94f8d44b2321b1ae3fad8e
unknown <Apoyo a Estudiantes@8A-DISE-CECY.uson.mx>
2022-11-11 19:01:43 -0700

    11 de noviembre de 2022

--- pygame/prueba.py
+++ pygame/prueba.py
@@ -7,8 +7,28 @@ import os #esto es para dirname
 
 
 
-dirname = os.path.dirname(__file__)
+dirname = os.path.dirname(__file__)+"/"
  
+
+class jugador:
+   
+
+   def __init__(self, position_x, position_y, imagen):
+      self.x = position_x
+      self.y = position_y
+      self.imagen = pygame.image.load(dirname+imagen)
+
+   def movimiento(self, pressed):
+      if pressed[pygame.K_w]:
+         self.y -= 2
+      if pressed[pygame.K_s]:
+         self.y += 2
+      if pressed[pygame.K_a]:
+         self.x -= 2
+      if pressed[pygame.K_d]:
+         self.x += 2
+
+
 
 #iniciadores
 pygame.init()
@@ -24,17 +44,22 @@ pygame.display.set_caption("Matar zorros")
  
 FramePerSec = pygame.time.Clock()
 
-x = 0
-y = 0
+
+
 
 rect = pygame.Rect(20,20,20,20)
 
 #imagenimage.png
 
-zorro = pygame.image.load(dirname+"/zorro2.png")
-fondo = pygame.image.load(dirname+"/fondo.png")
-persona = pygame.image.load(dirname+"/personayarma.png")
-bala = pygame.image.load(dirname+"/bala.png")
+#Player pero con clases, (oUwUo) : 
+
+player = jugador(100,100,"personayarma.png")
+
+fondo = pygame.image.load(dirname+"fondo.png")
+
+zorro = pygame.image.load(dirname+"zorro2.png")
+
+bala = pygame.image.load(dirname+"bala.png")
 
 #loop principal:
 
@@ -54,24 +79,20 @@ while True:
    #checa que teclas están oprimidas
    pressed = pygame.key.get_pressed()
 
-   #movimiento
-   if pressed[pygame.K_w]:
-      y -= 2
-   if pressed[pygame.K_s]:
-      y += 2
-   if pressed[pygame.K_a]:
-      x -= 2
-   if pressed[pygame.K_d]:
-      x += 2
-      if pressed[pygame.K_SPACE]:
-       x -= 2
+
+   #movimiento con clases 
+   player.movimiento(pressed)
 
 
    #renderizado
 
    displaysurface.blit(fondo, (0,0))
    displaysurface.blit(zorro, (150,150))
-   displaysurface.blit(persona, (x,y))
+
+
+   displaysurface.blit(player.imagen, (player.x,player.y))
+
+
    displaysurface.blit(bala, (15,15))
 
    #actualiza lo que se ve en ventana
